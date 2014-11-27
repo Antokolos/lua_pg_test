37b34c36adb404bf25e19fce66a8fdd29a7404fd
Antokolos <antokolos@gmail.com>
2014-11-28 00:16:35 +0600

    Platform android was deleted

--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/apkolosov/work/Azartox/lua_pg_test/platforms/android/ant-build/AndroidManifest.xml : \
-/home/apkolosov/work/Azartox/lua_pg_test/platforms/android/AndroidManifest.xml \
--- platforms/android/ant-build/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-/home/apkolosov/work/Azartox/lua_pg_test/platforms/android/ant-build/HelloWorld-debug-unaligned.apk : \
-/home/apkolosov/work/Azartox/lua_pg_test/platforms/android/ant-build/HelloWorld.ap_ \
-/home/apkolosov/work/Azartox/lua_pg_test/platforms/android/ant-build/classes.dex \
-/home/apkolosov/work/Azartox/lua_pg_test/platforms/android/CordovaLib/ant-build/classes.jar \
-/home/apkolosov/work/Azartox/lua_pg_test/platforms/android/libs/luaj-jse-3.0.jar \
