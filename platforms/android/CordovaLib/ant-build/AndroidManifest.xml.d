b75936dd8c00f1465e76203752944e0e20d9324b
Antokolos <antokolos@gmail.com>
2014-11-27 22:27:49 +0600

    Changing files were removed and added to ignore list

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/apkolosov/work/Azartox/lua_pg_test/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/apkolosov/work/Azartox/lua_pg_test/platforms/android/CordovaLib/AndroidManifest.xml \
